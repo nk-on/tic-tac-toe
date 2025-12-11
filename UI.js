f2d744a1fd05c57903fca0156eba117577307b39
nk-on <2nkoz@proton.me>
2025-12-11 21:13:35 +0400

    writing start game behaviour with toogle thus making code cleaner to read

--- UI.js
+++ UI.js
@@ -1,15 +1,8 @@
 const gameStartButton = document.querySelectorAll('.btn');
 const startMenu = document.querySelector('.start-menu');
 function gameStart(e) {
-    const btn = e.currentTarget;
-    if (btn.classList.contains('hidden')) {
-        startMenu.classList.remove('hidden');
-        startMenu.classList.add('visible')
-        return;
-    };
-    startMenu.classList.remove('visible');
-    startMenu.classList.add('hidden');
-
+    startMenu.classList.toggle('visible');
+    startMenu.classList.toggle('hidden');
 }
 gameStartButton.forEach((button) => {
     button.addEventListener('click', gameStart);
