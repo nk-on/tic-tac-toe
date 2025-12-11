0b41563bb76432fb0106325752d74b3786781b04
nk-on <2nkoz@proton.me>
2025-12-11 22:35:23 +0400

    Style:building heading of the main interface

--- UI.js
+++ UI.js
@@ -1,8 +1,11 @@
 const gameStartButton = document.querySelectorAll('.btn');
 const startMenu = document.querySelector('.start-menu');
+const gameMenu = document.querySelector('.game-menu');
 function gameStart(e) {
     startMenu.classList.toggle('visible');
     startMenu.classList.toggle('hidden');
+    gameMenu.classList.toggle('visible')
+    gameMenu.classList.toggle('hidden')
 }
 gameStartButton.forEach((button) => {
     button.addEventListener('click', gameStart);
