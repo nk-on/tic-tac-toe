54f32f985e6c3dc0bc0723018864d98bf6dcab95
nk-on <2nkoz@proton.me>
2025-12-11 21:10:38 +0400

    Feat:fixing the bug in particular game started when user started only one button

--- UI.js
+++ UI.js
@@ -1,11 +1,16 @@
-const gameStartButton = document.querySelector('.btn');
+const gameStartButton = document.querySelectorAll('.btn');
 const startMenu = document.querySelector('.start-menu');
-gameStartButton.addEventListener('click',()=>{
-    if(gameStartButton.classList.contains('hidden')){
+function gameStart(e) {
+    const btn = e.currentTarget;
+    if (btn.classList.contains('hidden')) {
         startMenu.classList.remove('hidden');
         startMenu.classList.add('visible')
         return;
     };
     startMenu.classList.remove('visible');
     startMenu.classList.add('hidden');
+
+}
+gameStartButton.forEach((button) => {
+    button.addEventListener('click', gameStart);
 })
