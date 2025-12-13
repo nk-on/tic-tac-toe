8c16a056745bdef912b6e7f8a6cb5cdae4266a2c
nk-on <2nkoz@proton.me>
2025-12-13 22:12:25 +0400

    switching the signs when user draws sign symbol into the square

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ const signData = [
     }
 ];
 let currentSign = signData[0];
+function chooseSign(e) {
+    const clickedSquare = e.currentTarget;
+    const id = clickedSquare.getAttribute('id');
+    currentSign = signData.find(data => data.signTitle === id);
+}
 function drawSquare(e) {
     const square = e.currentTarget;
     if (square.innerHTML.length >= 1) return;
     const {signImage} = currentSign;
     square.innerHTML += `<img src="${signImage}" />`;
-}
-function chooseSign(e) {
     currentSign = currentSign === signData[0] ? signData[1] : signData[0];
 }
 playerSign.forEach((sign) => {
