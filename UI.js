cdde135451687aaa11bd52fbb77dd46f49513831
nk-on <2nkoz@proton.me>
2025-12-15 22:10:56 +0400

    replacing hiding and showing elements via id's instead of hidden or visible classes

--- UI.js
+++ UI.js
@@ -1,12 +1,10 @@
-const gameStartButton = document.querySelectorAll('.btn');
+const gameStartButton = document.querySelectorAll('.start-button');
 const startMenu = document.querySelector('.start-menu');
 const gameMenu = document.querySelector('.game-menu');
 const gameGrid = document.querySelector('.game-grid');
 function gameStart(e) {
-    startMenu.classList.toggle('visible');
-    startMenu.classList.toggle('hidden');
-    gameMenu.classList.toggle('visible')
-    gameMenu.classList.toggle('hidden')
+    startMenu.id = "hidden";
+    gameMenu.id = "visible";
 }
 gameStartButton.forEach((button) => {
     button.addEventListener('click', gameStart);
