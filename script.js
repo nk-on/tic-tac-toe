e0358eec406bc8335029e609369e9f42ebd9c82d
nk-on <2nkoz@proton.me>
2025-12-26 21:09:03 +0400

    Feat:building system which determines the winner

--- script.js
+++ script.js
@@ -3,19 +3,57 @@ const oval = document.getElementById('oval');
 const gridSquares = document.querySelectorAll('.grid-square');
 const playerSign = document.querySelectorAll('.sign');
 const movesHistory = Array(16).fill(' ');
-console.log(movesHistory)
 const currentPlayerSign = document.getElementById('current-player-icon')
 const signData = [
     {
-        signTitle: 'cross',
+        playerName:'Player 1',
+        signTitle: 'X',
         signImage: 'assets/cross green.svg'
     },
     {
-        signTitle: 'oval',
+        playerName:'Player 2',
+        signTitle: 'O',
         signImage: 'assets/Oval orange.svg'
     }
 ];
+const overlay = document.querySelector('.overlay');
+const overlaySubtitle = document.querySelector('.subtitle')
+const winningCombos = [
+
+    // Rows
+    [0, 1, 2, 3], 
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+
+    // Columns
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+
+    // Diagonals
+    [0, 5, 10, 15],
+    [3, 6, 9, 12]
+
+];
+
 let currentSign = signData[0];
+function checkWinner() {
+    for (let i = 0; i < winningCombos.length; i++) {
+        const winCondition = winningCombos[i];
+        let a = movesHistory[winCondition[0]];
+        let b = movesHistory[winCondition[1]];
+        let c = movesHistory[winCondition[2]];
+        let d = movesHistory[winCondition[3]];
+        if ((a === b && b === c && d === c) && (a !== ' ' && b !== ' ' && c !== ' ' && d !== ' '
+        )) {
+            overlay.id = "visible";
+            return a;
+        }
+    }
+
+}
 function chooseSign(e) {
     const clickedSquare = e.currentTarget;
     const id = clickedSquare.getAttribute('id');
@@ -28,7 +66,7 @@ function switchSign() {
     currentSign = currentSign === signData[0] ? signData[1] : signData[0];
     insertCurrentPlayerSign();
 }
-function saveMove(id,signTitle) {
+function saveMove(id, signTitle) {
     movesHistory[id] = signTitle;
 }
 function insertSign(e) {
@@ -38,8 +76,8 @@ function insertSign(e) {
     const { signTitle, signImage } = currentSign;
     square.innerHTML += `<img src="${signImage}" />`;
     switchSign();
-    saveMove(id,signTitle);
-    console.log(movesHistory)
+    saveMove(id, signTitle);
+    checkWinner();
 }
 playerSign.forEach((sign) => {
     sign.addEventListener('click', chooseSign);
