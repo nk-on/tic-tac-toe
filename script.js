d0c51b9d905dd73ec6b9d9a94f32ef9f5fd97cb7
nk-on <2nkoz@proton.me>
2026-01-14 22:55:26 +0400

    Moving out winningCombos outside forloop

--- script.js
+++ script.js
@@ -90,26 +90,26 @@ function declareTie() {
     increaseScore();
 }
 function checkWinner() {
+    const winningCombos = [
+
+        // Rows
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+
+        // Columns
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15],
+
+        // Diagonals
+        [0, 5, 10, 15],
+        [3, 6, 9, 12]
+
+    ];
     for (let i = 0; i < winningCombos.length; i++) {
-        const winningCombos = [
-
-            // Rows
-            [0, 1, 2, 3],
-            [4, 5, 6, 7],
-            [8, 9, 10, 11],
-            [12, 13, 14, 15],
-
-            // Columns
-            [0, 4, 8, 12],
-            [1, 5, 9, 13],
-            [2, 6, 10, 14],
-            [3, 7, 11, 15],
-
-            // Diagonals
-            [0, 5, 10, 15],
-            [3, 6, 9, 12]
-
-        ];
         const winCondition = winningCombos[i];
         const a = movesHistory[winCondition[0]];
         const b = movesHistory[winCondition[1]];
