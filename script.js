35ba752cfee64dac6fc9f8f7d43e6007749a3f4c
nk-on <2nkoz@proton.me>
2026-01-15 21:27:40 +0400

    Feat:implementing function which makes move when user choses playing aganst computer

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ const playerScoreEl = document.querySelector('.player1-score h2');
 const totalTiesEl = document.querySelector('.total-ties h2');
 const computerScoreEl = document.querySelector('.player2-score h2');
 const imgEl = document.querySelector('.game-result img');
+const newGameVsCpuBtn = document.querySelector('.start-button.btn-yellow');
 let currentPlayer = playerData[0];
+let gameAgainstComputer = false;
 function clear() {
     gridSquares.forEach((square) => {
         square.replaceChildren();
@@ -90,26 +92,26 @@ function declareTie() {
     increaseScore();
 }
 function checkWinner() {
+    const winningCombos = [
+
+        // Rows
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+
+        // Columns
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15],
+
+        // Diagonals
+        [0, 5, 10, 15],
+        [3, 6, 9, 12]
+
+    ];
     for (let i = 0; i < winningCombos.length; i++) {
-        const winningCombos = [
-
-            // Rows
-            [0, 1, 2, 3],
-            [4, 5, 6, 7],
-            [8, 9, 10, 11],
-            [12, 13, 14, 15],
-
-            // Columns
-            [0, 4, 8, 12],
-            [1, 5, 9, 13],
-            [2, 6, 10, 14],
-            [3, 7, 11, 15],
-
-            // Diagonals
-            [0, 5, 10, 15],
-            [3, 6, 9, 12]
-
-        ];
         const winCondition = winningCombos[i];
         const a = movesHistory[winCondition[0]];
         const b = movesHistory[winCondition[1]];
@@ -142,6 +144,16 @@ function switchSign() {
 function saveMove(id, signTitle) {
     movesHistory[id] = signTitle;
 }
+function computerMove() {
+    let randIdx = Math.floor(Math.random() * movesHistory.length);
+    if (movesHistory[randIdx] !== ' ') {
+        while (movesHistory[randIdx] !== ' ') {
+            randIdx = Math.floor(Math.random() * movesHistory.length);
+        }
+    }
+    const { signImage } = currentPlayer;
+    gridSquares[randIdx].innerHTML = `<img src="${signImage}" />`;
+}
 function insertSign(e) {
     e.preventDefault();
     const square = e.currentTarget;
@@ -150,10 +162,13 @@ function insertSign(e) {
     const { signTitle, signImage } = currentPlayer;
     square.innerHTML += `<img src="${signImage}" />`;
     switchSign();
-
+    if (gameAgainstComputer) computerMove();
     saveMove(id, signTitle);
     checkWinner();
 }
+newGameVsCpuBtn.addEventListener('click', () => {
+    gameAgainstComputer = !gameAgainstComputer;
+})
 playerSign.forEach((sign) => {
     sign.addEventListener('click', choosePlayer);
 });
